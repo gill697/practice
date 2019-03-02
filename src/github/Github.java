2969326981ed3946241b240ccd60b7ba739f85fd
gill697 <igurmandeepgill@gmail.com>
2019-03-02 16:15:53 -0500

    initial commit.

---
+++ src/github/Github.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package github;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class Github {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hellow World");
+        
+    }
+    
+}
